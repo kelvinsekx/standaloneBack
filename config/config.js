8134ae0dfb9edb9c426b64bafd5844506abbefa0
kelvinsekx <utdkelvin@gmail.com>
2021-05-08 01:04:42 +0100

    seems working

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const config = {
     jwtSecret: process.env.JWT_SECRET || 'my_web_token',
     mongoUri: process.env.MONGOIDB_URI || 
 	process.env.MONGO_HOST || 
-	 "mongodb+srv://utdkelvin:Kukuejubola%401997@cluster0.zbmj6.mongodb.net/socmedia?retryWrites=true&w=majority"
+	 "mongodb+srv://utdkelvin:kukuejubola1997@cluster0.zbmj6.mongodb.net/socmedia?retryWrites=true&w=majority"
 	|| 'mongodb://' + (process.env.IP || 'localhost') + ':' + (process.env.MONGO_PORT || '27017') + '/mernproject'
 }
 
